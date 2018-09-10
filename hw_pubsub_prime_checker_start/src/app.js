80c8a9955fa0f491b04a92cb6ddea67ecbc74a23
Fofiko <foteinid@gmail>
2018-09-10 16:01:36 +0100

    Add require and exports across js files

--- hw_pubsub_prime_checker_start/src/app.js
+++ hw_pubsub_prime_checker_start/src/app.js
@@ -1,3 +1,8 @@
+const FormView = require('./views/form_view.js');
+const NumberChecker = require('./models/number_checker.js');
+const ResultView = require('./views/result_view.js');
+
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+  // console.log('JavaScript Loaded');
 });
---
+++ hw_pubsub_prime_checker_start/src/models/number_checker.js
@@ -0,0 +1,8 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const NumberChecker = function() {
+
+};
+
+
+module.exports = NumberChecker;
